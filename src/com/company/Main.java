b994c43303966da670084472d42b1a0d7c43610e
Viktor-ua <Pushkinvv@gmail.com>
2020-01-26 20:56:16 +0200

    message

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,10 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
         Employee emp1 = new Employee("John", "Show");
-
         String result = emp1.getInfo();
         System.out.println(result);
 
         emp1.work(8);
+
+        System.out.println("hello git");
     }
 }
